bfcf4f6a824d5e2ee5636a0e3a797d9e81ae6347
katie-hemus <katie.hemus@laterooms.com>
2016-09-27 10:34:32 +0100

    bumping metatdata

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "gocd"
 description      "Installs/Configures Go servers and agents"
 maintainer       "GoCD Contributors"
 maintainer_email "go-cd@googlegroups.com" 
-version          "1.1.2"
+version          "1.1.3"
 
 supports "ubuntu"
 supports "centos"
