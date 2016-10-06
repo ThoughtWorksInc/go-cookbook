9703e3a392091c471e1b3fd014d76d15c25f6a53
katie-hemus <katie.hemus@laterooms.com>
2016-10-06 09:47:46 +0100

    incrementing to go-server/agent version 6.10

--- attributes/repository.rb
+++ attributes/repository.rb
@@ -16,7 +16,7 @@ case node['gocd']['install_method']
 when 'repository'
 # version = nil so just pick latest available
 else
-  default['gocd']['version'] = '16.9.0-4001' 
+  default['gocd']['version'] = '16.10.0-4131' 
 end
 
 version = node['gocd']['version']
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             "gocd"
 description      "Installs/Configures Go servers and agents"
 maintainer       "GoCD Contributors"
 maintainer_email "go-cd@googlegroups.com" 
-version          "1.1.10"
+version          "1.1.11"
 
 supports "ubuntu"
 supports "centos"
